bb6ad0c55abdfbf24d9b3800db17ee72840d7fe4
aleksecmotin8-max <aleksecmotin8@gmail.com>
2026-07-05 23:39:17 +0300

    опять комичу не рабочий код надо было закомитить когда работает но уже устал завтра разберусь

--- Notes.js
+++ Notes.js
@@ -3,6 +3,7 @@ let state ={
     statePage : 'field',
    currentNoteId : null
 };
+
 const app = document.querySelector('#app');
 const createNote =(text,head)=>{
     let newNote = 
@@ -14,11 +15,144 @@ const createNote =(text,head)=>{
         pinned:false
     }
     state.notes.push(newNote);
+};
+const deleteContextMenu = ()=>{
+      const element = document.querySelector('#context-menu');
+      if(element!==null){
+      element.remove();}
+};
+const funkFieldMiniDiv =(arr,container)=>{
+   arr.forEach((item)=>{
+        const miniDiv = document.createElement('div');
+        const heading = document.createElement('h3')
+        const pText = document.createElement('p');
+        
+        heading.textContent = item.head.slice(0,100);
+        pText.textContent = item.text.slice(0,20);
+       
+        miniDiv.appendChild(heading);
+        miniDiv.appendChild(pText)
+       container.appendChild(miniDiv);
+
+        miniDiv.addEventListener('click',()=>{
+            state.statePage = 'edit';
+            state.currentNoteId = item.id
+           renderStatePage()
+          });
+           miniDiv.addEventListener('contextmenu',(event)=>{
+            event.preventDefault();
+            event.stopPropagation();
+            state.currentNoteId = item.id ;
+            showContextMenu(event)
+        });
+        
+     });
+      
+
+}
+const funkButtonSearch =()=>{
+    state.statePage = 'search';
+    renderStatePage()
 }
 const funkButtonPlus =()=>{
     state.statePage = 'add'
     renderStatePage()
 }
+const findById = (requiredId)=>{
+      return  state.notes.find((item)=>{
+        return item.id === requiredId
+       });
+}
+const funkButtonDelete =(requiredId)=>{
+       let findedObjIndex = state.notes.findIndex((item)=>{
+        return item.id === requiredId
+       })
+       state.notes.splice(findedObjIndex,1);
+    deleteContextMenu();
+       renderStatePage();
+};
+const funkbuttonFavorite = (requiredId) =>{
+       let findedObj =  findById(requiredId);
+     findedObj.favorite = !findedObj.favorite;
+     deleteContextMenu();
+     renderStatePage()
+}
+const funkButtonPinned = (requiredId) =>{
+       let findedObj =  findById(requiredId);
+       findedObj.pinned = !findedObj.pinned;
+      deleteContextMenu();
+       renderStatePage()
+}
+const showContextMenu =(event)=>{
+    deleteContextMenu();
+   let requiredId = state.currentNoteId;
+
+   let divContextMenu = document.createElement('div');
+   divContextMenu.id='context-menu'
+   
+   let buttonDelete = document.createElement('button');
+   let buttonPinned = document.createElement('button');
+   let buttonFavorite = document.createElement('button');
+
+   buttonDelete.textContent = 'Delete';
+   buttonPinned.textContent = 'Pinned';
+   buttonFavorite.textContent = 'Favorie'
+
+    divContextMenu.style.position = 'fixed';
+    divContextMenu.style.left = event.clientX + 'px';
+    divContextMenu.style.top = event.clientY + 'px';
+
+   buttonDelete.addEventListener('click',()=>funkButtonDelete(requiredId));
+   buttonPinned.addEventListener('click',()=>funkButtonPinned(requiredId));
+   buttonFavorite.addEventListener('click',()=>funkbuttonFavorite(requiredId));
+   divContextMenu.addEventListener('click',(event)=>{
+    event.stopPropagation();
+   })
+  
+   
+
+   divContextMenu.appendChild(buttonDelete);
+   divContextMenu.appendChild(buttonFavorite);
+   divContextMenu.appendChild(buttonPinned);
+   document.body.appendChild(divContextMenu);
+
+};
+
+const renderSearchScreen = ()=>{
+    let searchDiv = document.createElement('div');
+    let searchDivForMiniDiv = document.createElement('div')
+
+    let searchInput = document.createElement('input');
+
+    let buttonCancel = document.createElement('button');
+
+    buttonCancel.textContent = 'cancel';
+
+    searchDiv.appendChild(searchInput);
+    searchDiv.appendChild(searchDivForMiniDiv);
+    searchDiv.appendChild(buttonCancel);
+   
+    app.appendChild(searchDiv);
+
+    searchInput.addEventListener('input',()=>{
+       
+        searchDivForMiniDiv.replaceChildren()
+
+           let query =  searchInput.value.toLowerCase();
+    let filteredNotes= state.notes.filter((item)=>{
+        return item.text.toLowerCase().includes(query)
+    });
+   
+     searchDiv.appendChild(searchDivForMiniDiv);
+   funkFieldMiniDiv(filteredNotes,searchDivForMiniDiv);
+ 
+  
+    })
+   
+    
+
+}
+   
 const renderEditNotes = ()=>{
 let findedObj = state.notes.find((item)=>{
     return item.id === state.currentNoteId
@@ -28,7 +162,7 @@ let headingInput = document.createElement('input');
 let textArea = document.createElement('textarea');
 
 headingInput.value = findedObj.head;
-textArea.textContent = findedObj.text;
+textArea.value = findedObj.text;
 
 let buttonEscape = document.createElement('button');
 
@@ -39,14 +173,14 @@ let buttonEscape = document.createElement('button');
        state.statePage = 'field'
     let input = textArea.value.trim()
     if (input !== ''){
-        createNote(input,headingInput.value)
+       findedObj.text = textArea.value;
+       findedObj.head = headingInput.value 
     }
     renderStatePage()
    });
   editDiv.appendChild(buttonEscape);
   editDiv.appendChild(headingInput);
   editDiv.appendChild(textArea);
-  editDiv.appendChild(headingInput);
   app.appendChild(editDiv);
 };
 const renderAddNotes = () => {
@@ -79,35 +213,20 @@ const renderNotesField = ()=>{
     let divForMiniDiv = document.createElement('div')
 
     let buttonPlus = document.createElement('button');
+    let buttonSearch = document.createElement('button');
 
+    buttonSearch.textContent = ' search '
     buttonPlus.textContent = ' + ';
     
+    mainFieldDiv.appendChild(buttonSearch);
     mainFieldDiv.appendChild(buttonPlus);
+    mainFieldDiv.appendChild(divForMiniDiv)
     app.appendChild(mainFieldDiv);
 
-    state.notes.forEach((item)=>{
-        const miniDiv = document.createElement('div');
-        const heading = document.createElement('h3')
-        const pText = document.createElement('p');
-    
-        heading.textContent = item.head.slice(0,100);
-        pText.textContent = item.text.slice(0,20);
-       
-        miniDiv.appendChild(heading);
-        miniDiv.appendChild(pText)
-        divForMiniDiv.appendChild(miniDiv);
-         app.appendChild(divForMiniDiv)
-
-        miniDiv.addEventListener('click',()=>{
-            state.statePage = 'edit';
-            state.currentNoteId = item.id
-           renderStatePage()
-
-          });
-        
-     })
+   funkFieldMiniDiv(state.notes,divForMiniDiv);
 
     buttonPlus.addEventListener('click',funkButtonPlus);
+    buttonSearch.addEventListener('click',funkButtonSearch)
 }
 const renderStatePage = () =>{
         app.replaceChildren();
@@ -117,6 +236,13 @@ const renderStatePage = () =>{
         renderEditNotes()
     }else if (state.statePage ==='add'){
         renderAddNotes()
+ }else if (state.statePage==='search'){
+    renderSearchScreen()
  }
 }
-renderStatePage()
+document.addEventListener('contextmenu',(event)=>{
+   event.preventDefault();
+   deleteContextMenu();
+   })
+   
+renderStatePage()   
